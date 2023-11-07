20df8b04558894fbf145cc928b9ed3a9d7760f2e
olaponomareva <150163800+olaponomareva@users.noreply.github.com>
2023-11-07 19:40:32 +0300

    Add files via upload

---
+++ DestroyrScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyrScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //”ничтожение текущего обекта
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
